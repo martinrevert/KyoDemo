221444b45427e811e558aa21466bde7a94970dc1
martinrevert <martinrevert@gmail.com>
2018-05-29 18:09:25 -0300

    - Move Socket emit to Activity from Barcode class

--- app/src/main/java/io/webee/scanner/LivePreviewActivity.java
+++ app/src/main/java/io/webee/scanner/LivePreviewActivity.java
@@ -1,38 +1,77 @@
 package io.webee.scanner;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+
+
+import android.net.http.SslError;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+
+
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.example.google.nodejsmanager.nodejsmanager.ConnectionManager;
+import com.example.google.nodejsmanager.nodejsmanager.SocketManager;
 import com.google.android.gms.common.annotation.KeepName;
 import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.gson.JsonObject;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+import io.socket.client.IO;
+import io.socket.emitter.Emitter;
+import io.socket.engineio.client.transports.WebSocket;
 import io.webee.scanner.barcodescanning.BarcodeScanningProcessor;
+import io.webee.scanner.constants.Constants;
+
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
+
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+import android.webkit.SslErrorHandler;
+
+
+import static android.provider.ContactsContract.Directory.PACKAGE_NAME;
+import static java.util.concurrent.Executors.newScheduledThreadPool;
 
 
 @KeepName
 public final class LivePreviewActivity extends AppCompatActivity
         implements OnRequestPermissionsResultCallback,
         OnItemSelectedListener,
-        CompoundButton.OnCheckedChangeListener {
+        CompoundButton.OnCheckedChangeListener, ConnectionManager.EventCallbackListener, BarcodeScanningProcessor.onReadBarCodeListener {
     private static final String BARCODE_DETECTION = "Barcode Detection";
     private static final String TAG = "LivePreviewActivity";
     private static final int PERMISSION_REQUESTS = 1;
@@ -41,9 +80,49 @@ public final class LivePreviewActivity extends AppCompatActivity
     private CameraSourcePreview preview;
     private GraphicOverlay graphicOverlay;
     private String selectedModel = BARCODE_DETECTION;
+    private static final String EXTRA_CUSTOM_TABS_TOOLBAR_COLOR = "tabColor";
+    private WebView webView;
+    private CustomTabsClient mClient;
+    private String url;
+
 
     private Context context;
     private BarcodeScanningProcessor barcodeScanningProcessor;
+    private int connectionIntents = 0;
+
+    private ScheduledExecutorService schedulePingViot;
+    private Emitter.Listener onAuthenticated = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            Log.d(TAG, "VIoT - onAuthenticated");
+            socketManager.getSocket().emit("lb-ping");
+            if (schedulePingViot == null) {
+                schedulePingViot = newScheduledThreadPool(5);
+            }
+            schedulePingViot.scheduleAtFixedRate(new Runnable() {
+                public void run() {
+                    try {
+                        if (socketManager.getSocket() != null
+                                && socketManager.getSocket().connected()) {
+                            socketManager.getSocket().emit("lb-ping");
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, 0, 15, TimeUnit.SECONDS);
+        }
+    };
+    private Emitter.Listener onAndroidPongVIOT = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            if (socketManager.getSocket() != null) {
+                Log.d(TAG, "VIoT - pong received ...");
+            }
+        }
+    };
+
+    private SocketManager socketManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,18 +141,9 @@ public final class LivePreviewActivity extends AppCompatActivity
         if (graphicOverlay == null) {
             Log.d(TAG, "graphicOverlay is null");
         }
-
+        webView = (WebView) findViewById(R.id.webview);
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         List<String> options = new ArrayList<>();
-        Button btn_emit = (Button) (Button) findViewById(R.id.emit);
-        btn_emit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (barcodeScanningProcessor != null) {
-                    barcodeScanningProcessor.emitMessage();
-                }
-            }
-        });
 
         options.add(BARCODE_DETECTION);
 
@@ -98,6 +168,112 @@ public final class LivePreviewActivity extends AppCompatActivity
         } else {
             getRuntimePermissions();
         }
+
+        connectSocketViot();
+
+
+        url = "https://visual.webee.io/apps/view/5b0c45176e06475826e022a6/3bd115f0-d9fc-40e9-bb48-93bfcc6cdaeb";
+
+        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            warmUpChrome();
+            launchUrl();
+        } else {
+            setupWebView();
+        }
+    }
+
+    private void setupWebView() {
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setSaveFormData(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+
+        });
+        webView.loadUrl(url);
+    }
+
+    private void warmUpChrome() {
+        CustomTabsServiceConnection service = new CustomTabsServiceConnection() {
+            @Override
+            public void onCustomTabsServiceConnected(ComponentName name, CustomTabsClient client) {
+                mClient = client;
+                mClient.warmup(0);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                mClient = null;
+            }
+        };
+
+        CustomTabsClient.bindCustomTabsService(getApplicationContext(), PACKAGE_NAME, service);
+    }
+
+    private void launchUrl() {
+        Uri uri = Uri.parse(url);
+        if (uri == null) {
+            return;
+        }
+        CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder().
+                //setToolbarColor(getResources().getColor(R.color.control_background)).
+                        setShowTitle(true).build();
+        customTabsIntent.intent.setData(uri);
+
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> resolveInfoList = packageManager.queryIntentActivities(customTabsIntent.intent, PackageManager.MATCH_DEFAULT_ONLY);
+
+        for (ResolveInfo resolveInfo : resolveInfoList) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            if (TextUtils.equals(packageName, PACKAGE_NAME))
+                customTabsIntent.intent.setPackage(PACKAGE_NAME);
+        }
+
+        customTabsIntent.launchUrl(this, uri);
+    }
+
+
+    private void connectSocketViot() {
+        if (connectionIntents > 3) {
+            showErrorMessage();
+            finish();
+            return;
+        }
+        connectionIntents++;
+        socketManager = new SocketManager(context);
+        IO.Options opts = new IO.Options();
+        opts.transports = new String[]{WebSocket.NAME};
+        //opts.forceNew = true;
+        socketManager.createSocket(Constants.VIOT_BASE_URL, opts);
+
+
+        socketManager.getSocket().on("authenticated", onAuthenticated);
+        socketManager.getSocket().on("lb-pong", onAndroidPongVIOT);
+
+
+        if (socketManager.getSocket().connected()) {
+            socketManager.getSocket().disconnect();
+        }
+        socketManager.getSocket().connect();
+    }
+
+    private void showErrorMessage() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, "Socket disconnected", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -147,7 +323,7 @@ public final class LivePreviewActivity extends AppCompatActivity
         switch (model) {
             case BARCODE_DETECTION:
                 Log.i(TAG, "Using Barcode Detector Processor");
-                barcodeScanningProcessor = new BarcodeScanningProcessor(context);
+                barcodeScanningProcessor = new BarcodeScanningProcessor(this);
                 cameraSource.setMachineLearningFrameProcessor(barcodeScanningProcessor);
                 break;
 
@@ -184,6 +360,8 @@ public final class LivePreviewActivity extends AppCompatActivity
         super.onResume();
         Log.d(TAG, "onResume");
         startCameraSource();
+        BarcodeScanningProcessor.subscribeToListener(this);
+        ConnectionManager.subscribeToListener(this);
     }
 
     /**
@@ -192,7 +370,16 @@ public final class LivePreviewActivity extends AppCompatActivity
     @Override
     protected void onPause() {
         super.onPause();
+        BarcodeScanningProcessor.unSubscribeToListener();
+        ConnectionManager.unSubscribeToListener();
         preview.stop();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (socketManager.getSocket() != null)
+            socketManager.getSocket().disconnect();
     }
 
     @Override
@@ -264,5 +451,65 @@ public final class LivePreviewActivity extends AppCompatActivity
         }
         Log.i(TAG, "Permission NOT granted: " + permission);
         return false;
+    }
+
+    static JSONObject getCredentials() {
+        try {
+            String secondPart = "/api/connections/generateToken?api_key=%s&api_secret=%s";
+            String[] APIs = new String[]{Constants.API_KEY, Constants.API_SECRET};
+            String generateTokenApi = Constants.VIOT_BASE_URL + secondPart;
+            URL url = new URL(String.format(generateTokenApi, APIs[0],
+                    APIs[1]));
+            HttpURLConnection connection =
+                    (HttpURLConnection) url.openConnection();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+            StringBuilder json = new StringBuilder(1024);
+            String tmp;
+            while ((tmp = reader.readLine()) != null)
+                json.append(tmp).append("\n");
+            reader.close();
+            return new JSONObject(json.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void onEventCallbackReceived(String event, String socketIdentifier) {
+        switch (event) {
+            case ConnectionManager.EVENT_CONNECT: {
+                Log.d(TAG, "VIoT - onConnectEvent");
+                if (socketManager.getSocket() != null) {
+                    JSONObject json = getCredentials();
+                    try {
+                        if (json != null) {
+                            JSONObject requestJSONObject = new JSONObject();
+                            requestJSONObject.put("id", json.getString("id"));
+                            requestJSONObject.put("connectionId", json.getString("connectionId"));
+                            requestJSONObject.put("agent", "hub");
+                            requestJSONObject.put("uuid", "ACDBDA37565C");
+                            socketManager.getSocket().emit("webee-auth-strategy", requestJSONObject);
+                            Log.i(TAG, "json: " + requestJSONObject);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            }
+            case ConnectionManager.EVENT_DISCONNECT: {
+                Log.d(TAG, "VIoT - onDisconnectEvent");
+                connectSocketViot();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onReadBarCode(JsonObject message) {
+        Log.v(TAG, "message" + message);
+        socketManager.getSocket().emit("webee-hub-logger", message);
     }
 }
--- app/src/main/java/io/webee/scanner/barcodescanning/BarcodeScanningProcessor.java
+++ app/src/main/java/io/webee/scanner/barcodescanning/BarcodeScanningProcessor.java
@@ -2,72 +2,49 @@ package io.webee.scanner.barcodescanning;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
-import com.example.google.nodejsmanager.nodejsmanager.ConnectionManager;
-import com.example.google.nodejsmanager.nodejsmanager.SocketManager;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
-import java.util.Timer;
-import java.util.concurrent.TimeUnit;
 
-import io.socket.client.IO;
-import io.socket.emitter.Emitter;
-import io.socket.engineio.client.transports.WebSocket;
 import io.webee.scanner.FrameMetadata;
 import io.webee.scanner.GraphicOverlay;
+import io.webee.scanner.R;
 import io.webee.scanner.VisionProcessorBase;
-import io.webee.scanner.constants.Constants;
 
 
-public class BarcodeScanningProcessor extends VisionProcessorBase<List<FirebaseVisionBarcode>> implements ConnectionManager.EventCallbackListener {
+public class BarcodeScanningProcessor extends VisionProcessorBase<List<FirebaseVisionBarcode>> {
 
     private static final String TAG = "BarcodeScanProc";
 
     private final FirebaseVisionBarcodeDetector detector;
-    private final Context context;
-    private boolean authenticated = false;
-    private Timer schedulePing;
-    private Emitter.Listener onAuthenticated = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            authenticated = true;
-            Log.d(TAG, "VIoT - onAuthenticated");
-            socketManager.getSocket().emit("webee-hub-logger", "Hola mundo");
-//            socketManager.getSocket().emit("lb-ping");
-//            if (schedulePing == null) {
-//                schedulePing = newScheduledThreadPool(5);
-//            }
-//            schedulePing.scheduleAtFixedRate(new Runnable() {
-//                public void run() {
-//                    try {
-//                        if ( socketManager.getSocket() != null
-//                                &&  socketManager.getSocket().connected()) {
-//                            socketManager.getSocket().emit("lb-ping");
-//                        }
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }, 0, 15, TimeUnit.SECONDS);
-        }
-    };
+    private static onReadBarCodeListener onReadBarCodeListener;
+    private MediaPlayer mp;
+    private Context context;
 
-    private SocketManager socketManager;
+    public interface onReadBarCodeListener {
+        public void onReadBarCode(JsonObject eventLog);
+    }
+
+    public static void subscribeToListener(onReadBarCodeListener listener) {
+        onReadBarCodeListener = listener;
+    }
+
+    public static void unSubscribeToListener() {
+        onReadBarCodeListener = null;
+    }
+
 
     public BarcodeScanningProcessor(Context context) {
         // Note that if you know which format of barcode your app is dealing with, detection will be
@@ -75,32 +52,13 @@ public class BarcodeScanningProcessor extends VisionProcessorBase<List<FirebaseV
         // new FirebaseVisionBarcodeDetectorOptions.Builder()
         //     .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE)
         //     .build();
-
-        this.context = context;
-        ConnectionManager.subscribeToListener(this);
         detector = FirebaseVision.getInstance().getVisionBarcodeDetector();
-
-        socketManager = new SocketManager(context);
-        IO.Options opts = new IO.Options();
-        opts.transports = new String[]{WebSocket.NAME};
-        socketManager.createSocket(Constants.VIOT_BASE_URL_ALTERNATIVE, opts);
-
-
-        socketManager.getSocket().on("authenticated", onAuthenticated);
-
-
-        if (socketManager.getSocket().connected()) {
-            socketManager.getSocket().disconnect();
-        }
-        socketManager.getSocket().connect();
-
-
+        this.context = context;
     }
 
     @Override
     public void stop() {
         try {
-            ConnectionManager.unSubscribeToListener();
             detector.close();
         } catch (IOException e) {
             Log.e(TAG, "Exception thrown while trying to close Barcode Detector: " + e);
@@ -124,28 +82,31 @@ public class BarcodeScanningProcessor extends VisionProcessorBase<List<FirebaseV
             BarcodeGraphic barcodeGraphic = new BarcodeGraphic(graphicOverlay, barcode);
             graphicOverlay.add(barcodeGraphic);
 
-            //ToDo Here we have to call to VIOT and we have to notify the Activity to start a WebView
+            //ToDo MediaPlyer sonido de scaneo
+            mp = MediaPlayer.create(context, R.raw.fuzzybeep);
+            mp.start();
 
             Log.v("BarcodeScanningProcessor", "Scan succesful");
 
+            JsonObject message = new JsonObject();
+            try {
 
-//            if (!authenticated) {
-//                Log.v("BarcodeScanningProcessor", "no athennticatred");
-//                return;
-//            }
-//            JSONObject message = new JSONObject();
-//            try {
-//                message.put("deviceId", "0011223344558899");
-//                message.put("deviceName", "SCANNER");
-//                message.put("protocol", "zigbee");
-//                message.put("barcode", barcode.getDisplayValue());
-//                message.put("user", "Martin");
-//            } catch (JSONException e) {
-//                e.printStackTrace();
-//            }
-//            socketManager.getSocket().emit("webee-hub-logger", "Hola mundo");
-//            socketManager.getSocket().emit("webee-hub-logger", message);
+                //message.addProperty("deviceId", "0011223344558899");
+                message.addProperty("deviceId", "0011223344556677");
+                message.addProperty("deviceName", "ASHRAE");
+                //  message.addProperty("deviceName", "SCANNER");
 
+                message.addProperty("protocol", "zigbee");
+                //message.addProperty("barcode", barcode.getDisplayValue());
+                //message.addProperty("user", "Martin");
+                message.addProperty("quality",80);
+
+            } catch (JsonIOException e) {
+                e.printStackTrace();
+            }
+
+            if (onReadBarCodeListener != null)
+                onReadBarCodeListener.onReadBarCode(message);
         }
     }
 
@@ -154,64 +115,4 @@ public class BarcodeScanningProcessor extends VisionProcessorBase<List<FirebaseV
         Log.e(TAG, "Barcode detection failed " + e);
     }
 
-    static JSONObject getCredentials() {
-        try {
-            String secondPart = "/api/connections/generateToken?api_key=%s&api_secret=%s";
-            String[] APIs = new String[]{Constants.API_KEY, Constants.API_SECRET};
-            String generateTokenApi = Constants.VIOT_BASE_URL_ALTERNATIVE + secondPart;
-            URL url = new URL(String.format(generateTokenApi, APIs[0],
-                    APIs[1]));
-            HttpURLConnection connection =
-                    (HttpURLConnection) url.openConnection();
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream()));
-            StringBuilder json = new StringBuilder(1024);
-            String tmp;
-            while ((tmp = reader.readLine()) != null)
-                json.append(tmp).append("\n");
-            reader.close();
-            return new JSONObject(json.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    @Override
-    public void onEventCallbackReceived(String event, String socketIdentifier) {
-        switch (event) {
-            case ConnectionManager.EVENT_CONNECT: {
-
-                Log.d(TAG, "VIoT - onConnectEvent");
-                if (socketManager.getSocket() != null) {
-                    JSONObject json = getCredentials();
-                    try {
-                        if (json != null) {
-                            JSONObject requestJSONObject = new JSONObject();
-                            requestJSONObject.put("id", json.getString("id"));
-                            requestJSONObject.put("connectionId", json.getString("connectionId"));
-                            requestJSONObject.put("agent", "hub");
-                            requestJSONObject.put("uuid", "ACDBDA37565C");
-                            socketManager.getSocket().emit("webee-auth-strategy", requestJSONObject);
-                            Log.i(TAG, "json: "+requestJSONObject);
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-
-                }
-
-                break;
-            }
-            case ConnectionManager.EVENT_DISCONNECT:{
-                Log.d(TAG, "VIoT - onDisconnectEvent");
-                break;
-            }
-        }
-    }
-
-    public void emitMessage() {
-        socketManager.getSocket().emit("webee-hub-logger", "Tonchi se come la banana");
-    }
 }
